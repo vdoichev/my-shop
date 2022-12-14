bf0b3930434052f4182203143913e3c7b28a3a7f
Дойчев Вячеслав <40699294+vdoichev@users.noreply.github.com>
2022-12-15 00:29:05 +0200

    Add controller for get all products

---
+++ src/main/java/com/vdoichev/myshop/controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.vdoichev.myshop.controller;
+
+import com.vdoichev.myshop.dto.ProductDTO;
+import com.vdoichev.myshop.facade.ProductFacade;
+import com.vdoichev.myshop.services.ProductService;
+import com.vdoichev.myshop.validations.ResponseErrorValidation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("api/shop/product")
+@CrossOrigin
+public class ProductController {
+    @Autowired
+    private ProductFacade productFacade;
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private ResponseErrorValidation responseErrorValidation;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<ProductDTO>> getAllProducts() {
+        List<ProductDTO> productDTOList = productService.getAllProducts()
+                .stream()
+                .map(productFacade::productToProductDto)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(productDTOList, HttpStatus.OK);
+    }
+}
